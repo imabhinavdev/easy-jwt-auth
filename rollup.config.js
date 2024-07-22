d091d204ba55d9decb3bbbee03994f17da3a41ec
Abhinav Singh <abhinavas430@gmail.com>
2024-07-22 23:10:59 +0530

    chore: Update npm dependencies and add terser plugin for minification

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,24 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import terser from '@rollup/plugin-terser';
+import { defineConfig } from 'rollup';
 
-export default {
-    input: 'index.js', // Entry file
+// Define different configurations for development and production
+const isProduction = process.env.NODE_ENV === 'production';
+
+export default defineConfig({
+    input: 'index.js', // Adjusted path if necessary
     output: {
-        dir: 'dist', // Output directory
-        // file: 'dist/bundle.js', // Output file
+        dir: 'dist',
         format: 'es', // Output format: ES modules
-        sourcemap: true, // Generate source maps
+        sourcemap: false, // Enable source maps only in development
     },
     plugins: [
         resolve(), // Resolve modules from node_modules
         commonjs(), // Convert CommonJS modules to ES6
+        terser() // Minify the output in production
     ],
-};
+    treeshake: {
+        moduleSideEffects: false // Enable tree shaking to remove unused code
+    },
+});
