78cca4e401e6ad91e56e469fda6c340f152470f8
Abhinav Singh <abhinavas430@gmail.com>
2024-08-02 09:20:09 +0530

    chore: Refactor authMiddleware to improve path matching

--- utils/authMiddleware.js
+++ utils/authMiddleware.js
@@ -1,11 +1,15 @@
 import jwt from 'jsonwebtoken';
-import minimatch from 'minimatch';
 
 // utils/authMiddleware.js
 export const authMiddleware = ({ secret, except = [], algorithms = ["HS256"] }) => {
     return (req, res, next) => {
+        // Log request path for debugging
+        console.log(`Request Path: ${req.path}`);
+
         // Check if the request path matches any pattern in the except array
-        const isExcluded = except.some(pattern => minimatch(req.path, pattern));
+        const isExcluded = except.some(pattern => pathMatches(req.path, pattern));
+        console.log(`Is Excluded: ${isExcluded}`); // Log if the path is excluded
+
         if (isExcluded) {
             return next(); // Skip authentication for excluded routes
         }
@@ -25,4 +29,9 @@ export const authMiddleware = ({ secret, except = [], algorithms = ["HS256"] }) 
             res.status(401).json({ message: 'Invalid token' });
         }
     };
+};
+
+const pathMatches = (path, pattern) => {
+    const regex = new RegExp(`^${pattern.replace(/\*/g, '.*')}$`);
+    return regex.test(path);
 };
